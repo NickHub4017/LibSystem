b91395f67de3be19cf47b286f19ff83e95eb3289
Shaneqq <chandimaarangala@gmail.com>
2016-12-27 21:58:59 +0530

    admin template initial commit

--- htdocs/LibraryManagement/DbConnection/DBLink.php
+++ htdocs/LibraryManagement/DbConnection/DBLink.php
@@ -1,28 +1,27 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: NRV
  * Date: 11/14/2016
  * Time: 9:15 AM
  */
-
 class DBLink {
-var $username = "root";
+
+    var $username = "root";
     var $password = "";
     var $hostname = "localhost";
 
-
-    public function reguser(User $user)
-    {
+    public function reguser(User $user) {
         //$dbhandle = mysql_connect($this->hostname, $this->username, $this->password);
         //$selected = mysql_select_db("libsystem",$dbhandle);
-        if($this->checkUser($user)){
+        if ($this->checkUser($user)) {
             return "User Exsists";
         }
-        $con=mysqli_connect($this->hostname, $this->username, $this->password,"libsystem");
+        $con = mysqli_connect($this->hostname, $this->username, $this->password, "libsystem");
         $sql = 'INSERT INTO User (uname, pwd, address, telno, name, email,type) VALUES ( ? , ? , ? , ? , ? , ?,? );';
         $stmt = $con->prepare($sql);
-        $stmt->bind_param('sssssss', $user->getUname(),$user->getPwd(),$user->getAddress(),$user->getTelno(), $user->getName(),$user->getEmail(),$user->getType());
+        $stmt->bind_param('sssssss', $user->getUname(), $user->getPwd(), $user->getAddress(), $user->getTelno(), $user->getName(), $user->getEmail(), $user->getType());
 
         if ($stmt->execute()) {
             mysqli_close($con);
@@ -31,46 +30,40 @@ var $username = "root";
             mysqli_close($con);
             return $con->error;
         }
-
-
     }
 
-    public function login(User $user)
-    {
+    public function login(User $user) {
         //$dbhandle = mysql_connect($this->hostname, $this->username, $this->password);
         //$selected = mysql_select_db("libsystem",$dbhandle);
-        $con=mysqli_connect($this->hostname, $this->username, $this->password,"libsystem");
+        $con = mysqli_connect($this->hostname, $this->username, $this->password, "libsystem");
         $sql = 'SELECT * FROM User where uname = ? and  pwd = ?;';
 
         $stmt = $con->prepare($sql);
 
-        $stmt->bind_param('ss', $user->getUname(),$user->getPwd());
+        $stmt->bind_param('ss', $user->getUname(), $user->getPwd());
 
 
-        $no_rows=0;
+        $no_rows = 0;
         $stmt->execute();
 
-        $returned_name=null;
-        $returned_name2=null;
-        $result=$stmt->get_result();
-        $rows=$result->fetch_all();
+        $returned_name = null;
+        $returned_name2 = null;
+        $result = $stmt->get_result();
+        $rows = $result->fetch_all();
 
 
-        if(sizeof($rows)==1){
+        if (sizeof($rows) == 1) {
             mysqli_close($con);
             return $rows[0];
         }
         mysqli_close($con);
         return false;
-
-
     }
 
-    public function checkUser(User $user)
-    {
+    public function checkUser(User $user) {
         //$dbhandle = mysql_connect($this->hostname, $this->username, $this->password);
         //$selected = mysql_select_db("libsystem",$dbhandle);
-        $con=mysqli_connect($this->hostname, $this->username, $this->password,"libsystem");
+        $con = mysqli_connect($this->hostname, $this->username, $this->password, "libsystem");
         $sql = 'SELECT * FROM User where uname = ?;';
 
         $stmt = $con->prepare($sql);
@@ -78,36 +71,33 @@ var $username = "root";
         $stmt->bind_param('s', $user->getUname());
 
 
-        $no_rows=0;
+        $no_rows = 0;
         $stmt->execute();
 
-        $returned_name=null;
-        $returned_name2=null;
-        $result=$stmt->get_result();
-        $rows=$result->fetch_object();
+        $returned_name = null;
+        $returned_name2 = null;
+        $result = $stmt->get_result();
+        $rows = $result->fetch_object();
 
 
-        if(sizeof($rows)!=0){
+        if (sizeof($rows) != 0) {
             mysqli_close($con);
             return true;
         }
         mysqli_close($con);
         return false;
-
-
     }
 
-    public function bookAddToDB(Book $book)
-    {
+    public function bookAddToDB(Book $book) {
         //$dbhandle = mysql_connect($this->hostname, $this->username, $this->password);
         //$selected = mysql_select_db("libsystem",$dbhandle);
-        if($this->checkBook($book)){
+        if ($this->checkBook($book)) {
             return "Book Exsists";
         }
-        $con=mysqli_connect($this->hostname, $this->username, $this->password,"libsystem");
+        $con = mysqli_connect($this->hostname, $this->username, $this->password, "libsystem");
         $sql = 'INSERT INTO book (bookname, ISBN, Author, Edition) VALUES ( ? , ? , ? , ? );';
         $stmt = $con->prepare($sql);
-        $stmt->bind_param('ssss',$book->getName(),$book->getISBN(),$book->getAuthor(),$book->getEdition() );
+        $stmt->bind_param('ssss', $book->getName(), $book->getISBN(), $book->getAuthor(), $book->getEdition());
 
         if ($stmt->execute()) {
             mysqli_close($con);
@@ -116,14 +106,30 @@ var $username = "root";
             mysqli_close($con);
             return $con->error;
         }
-
     }
 
-    public function checkBook(Book $book)
-    {
+    public function copyBookAddToDB(CopyBook $book) {
         //$dbhandle = mysql_connect($this->hostname, $this->username, $this->password);
         //$selected = mysql_select_db("libsystem",$dbhandle);
-        $con=mysqli_connect($this->hostname, $this->username, $this->password,"libsystem");
+
+        $con = mysqli_connect($this->hostname, $this->username, $this->password, "libsystem");
+        $sql = 'INSERT INTO copyBook (BookID,BookType) VALUES ( ? , ? );';
+        $stmt = $con->prepare($sql);
+        $stmt->bind_param('ss', $book->getBookdata(), $book->getTypeBook());
+
+        if ($stmt->execute()) {
+            mysqli_close($con);
+            return "New Copy Book created successfully";
+        } else {
+            mysqli_close($con);
+            return $con->error;
+        }
+    }
+
+    public function checkBook(Book $book) {
+        //$dbhandle = mysql_connect($this->hostname, $this->username, $this->password);
+        //$selected = mysql_select_db("libsystem",$dbhandle);
+        $con = mysqli_connect($this->hostname, $this->username, $this->password, "libsystem");
         $sql = 'SELECT * FROM book where bookname = ?;';
 
         $stmt = $con->prepare($sql);
@@ -131,30 +137,27 @@ var $username = "root";
         $stmt->bind_param('s', $book->getISBN());
 
 
-        $no_rows=0;
+        $no_rows = 0;
         $stmt->execute();
 
-        $returned_name=null;
-        $returned_name2=null;
-        $result=$stmt->get_result();
-        $rows=$result->fetch_object();
+        $returned_name = null;
+        $returned_name2 = null;
+        $result = $stmt->get_result();
+        $rows = $result->fetch_object();
 
 
-        if(sizeof($rows)!=0){
+        if (sizeof($rows) != 0) {
             mysqli_close($con);
             return true;
         }
         mysqli_close($con);
         return false;
-
-
     }
 
-    public function getAllAuthors()
-    {
+    public function getAllAuthors() {
         //$dbhandle = mysql_connect($this->hostname, $this->username, $this->password);
         //$selected = mysql_select_db("libsystem",$dbhandle);
-        $con=mysqli_connect($this->hostname, $this->username, $this->password,"libsystem");
+        $con = mysqli_connect($this->hostname, $this->username, $this->password, "libsystem");
         $sql = 'SELECT * FROM author';
 
         $stmt = $con->prepare($sql);
@@ -162,20 +165,245 @@ var $username = "root";
 
 
 
-        $no_rows=0;
+        $no_rows = 0;
         $stmt->execute();
 
-        $returned_name=null;
-        $returned_name2=null;
-        $result=$stmt->get_result();
-        $rows=$result->fetch_all();
+        $returned_name = null;
+        $returned_name2 = null;
+        $result = $stmt->get_result();
+        $rows = $result->fetch_all();
 
 
         mysqli_close($con);
         return $rows;
-
-
     }
 
+    public function checkAuthor(Author $author) {
+        //$dbhandle = mysql_connect($this->hostname, $this->username, $this->password);
+        //$selected = mysql_select_db("libsystem",$dbhandle);
+        $con = mysqli_connect($this->hostname, $this->username, $this->password, "libsystem");
+        $sql = 'SELECT * FROM Author where AuthorName = ?;';
 
-} 
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param('s', $author->getAuthor());
+
+
+        $no_rows = 0;
+        $stmt->execute();
+
+        $returned_name = null;
+        $returned_name2 = null;
+        $result = $stmt->get_result();
+        $rows = $result->fetch_object();
+
+
+        if (sizeof($rows) != 0) {
+            mysqli_close($con);
+            return true;
+        }
+        mysqli_close($con);
+        return false;
+    }
+
+    public function AuthorAddToDB(Author $author) {
+        //$dbhandle = mysql_connect($this->hostname, $this->username, $this->password);
+        //$selected = mysql_select_db("libsystem",$dbhandle);
+        if ($this->checkAuthor($author)) {
+            return "Author Exsists";
+        }
+        $con = mysqli_connect($this->hostname, $this->username, $this->password, "libsystem");
+        $sql = 'INSERT INTO Author (AuthorName) VALUES ( ? );';
+        $stmt = $con->prepare($sql);
+        $stmt->bind_param('s', $author->getAuthor());
+
+        if ($stmt->execute()) {
+            mysqli_close($con);
+            return "New Author Added to database successfully";
+        } else {
+            mysqli_close($con);
+            return $con->error;
+        }
+    }
+
+    public function getAllBooks() {
+        //$dbhandle = mysql_connect($this->hostname, $this->username, $this->password);
+        //$selected = mysql_select_db("libsystem",$dbhandle);
+        $con = mysqli_connect($this->hostname, $this->username, $this->password, "libsystem");
+        $sql = 'SELECT * FROM book inner join author on book.Author  = author.AuthID';
+
+        $stmt = $con->prepare($sql);
+
+
+
+
+        $no_rows = 0;
+        $stmt->execute();
+
+        $returned_name = null;
+        $returned_name2 = null;
+        $result = $stmt->get_result();
+        $rows = $result->fetch_all();
+
+
+        mysqli_close($con);
+        return $rows;
+    }
+
+    public function checkMember(LendBook $lendBook) {
+        //$dbhandle = mysql_connect($this->hostname, $this->username, $this->password);
+        //$selected = mysql_select_db("libsystem",$dbhandle);
+        $con = mysqli_connect($this->hostname, $this->username, $this->password, "libsystem");
+        $sql = 'SELECT * FROM memship where memship_id = ?;';
+
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param('d', $lendBook->getMembership_id());
+
+
+        $no_rows = 0;
+        $stmt->execute();
+
+        $returned_name = null;
+        $returned_name2 = null;
+        $result = $stmt->get_result();
+        $rows = $result->fetch_object();
+
+
+        if (sizeof($rows) != 0) {
+            mysqli_close($con);
+            return true;
+        }
+        mysqli_close($con);
+        return false;
+    }
+
+    public function checkBookAvailable(LendBook $lendBook) {
+        //$dbhandle = mysql_connect($this->hostname, $this->username, $this->password);
+        //$selected = mysql_select_db("libsystem",$dbhandle);
+        $con = mysqli_connect($this->hostname, $this->username, $this->password, "libsystem");
+        $sql = 'SELECT * FROM lendingrecord where CopybookID = ? and isReturned = false ;';
+
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param('d', $lendBook->getCopyid());
+
+
+        $no_rows = 0;
+        $stmt->execute();
+
+        $returned_name = null;
+        $returned_name2 = null;
+        $result = $stmt->get_result();
+        $rows = $result->fetch_object();
+
+
+        if (sizeof($rows) != 0) {
+            mysqli_close($con);
+            return true;
+        }
+        mysqli_close($con);
+        return false;
+    }
+
+    public function checkBookReserve(LendBook $lendBook) {
+        //$dbhandle = mysql_connect($this->hostname, $this->username, $this->password);
+        //$selected = mysql_select_db("libsystem",$dbhandle);
+        $con = mysqli_connect($this->hostname, $this->username, $this->password, "libsystem");
+        $sql = 'SELECT * FROM reservation where copybook_id = ? and isReserved = true ;';
+
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param('d', $lendBook->getCopyid());
+
+
+        $no_rows = 0;
+        $stmt->execute();
+
+        $returned_name = null;
+        $returned_name2 = null;
+        $result = $stmt->get_result();
+        $rows = $result->fetch_object();
+
+
+        if (sizeof($rows) != 0) {
+            mysqli_close($con);
+            return true;
+        }
+        mysqli_close($con);
+        return false;
+    }
+
+    public function checkBookCondition(LendBook $lendBook) {
+        //$dbhandle = mysql_connect($this->hostname, $this->username, $this->password);
+        //$selected = mysql_select_db("libsystem",$dbhandle);
+        $con = mysqli_connect($this->hostname, $this->username, $this->password, "libsystem");
+        $sql = 'SELECT * FROM copybook where idCopyBook = ? ;';
+
+        $stmt = $con->prepare($sql);
+
+        $stmt->bind_param('d', $lendBook->getCopyid());
+
+
+        $no_rows = 0;
+        $stmt->execute();
+
+        $returned_name = null;
+        $returned_name2 = null;
+        $result = $stmt->get_result();
+        $rows = $result->fetch_object();
+
+
+        if (sizeof($rows) != 0) {
+            mysqli_close($con);
+            return $rows;
+        }
+        mysqli_close($con);
+        return false;
+    }
+
+    public function LendBookAddToDB(LendBook $lendBook) {
+        //$dbhandle = mysql_connect($this->hostname, $this->username, $this->password);
+        //$selected = mysql_select_db("libsystem",$dbhandle);
+        if ($lendBook->getUserType() == "te") {
+            
+        } else {
+            if ($this->checkMember($lendBook)) {
+                if (!$this->checkBookAvailable($lendBook)) {
+                    if (!$this->checkBookReserve($lendBook)) {
+                        $rows = $this->checkBookCondition($lendBook);
+                        if ($rows[2] == "dam") {
+                            return "Book is damaged";
+                        } else {
+                            $con = mysqli_connect($this->hostname, $this->username, $this->password, "libsystem");
+                            $sql = 'INSERT INTO lendingrecord (CopybookID,memship_id) VALUES ( ?,? );';
+                            $stmt = $con->prepare($sql);
+                            $stmt->bind_param('s', $lendBook->getCopyid());
+                            $stmt->bind_param('d', $lendBook->getMembership_id());
+
+                            if ($stmt->execute()) {
+                                mysqli_close($con);
+                                if($rows[2] == "ref"){
+                                    return "Lending Successfully Book is reference type";
+                                }else if($rows[2] == "len"){
+                                     return "Lending Successfully Book is lend type";
+                                }
+                                
+                            } else {
+                                mysqli_close($con);
+                                return $con->error;
+                            }
+                        }
+                    } else {
+                        return "Book is reserved";
+                    }
+                } else {
+                    return "Book is not available";
+                }
+            } else {
+                return "Invalid Member ID";
+            }
+        }
+    }
+
+}
